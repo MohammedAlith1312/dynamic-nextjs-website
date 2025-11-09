838487ab8cd5e704c156fa5d4be0f2b83a27d6ca
mohammedalith <mohammedalith713@gmail.com>
2025-11-09 12:15:48 +0530

    usecache

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig:NextConfig = {
-  // output: 'export', // 👈 enables static export
-  // images: {
-  //   unoptimized: true, // Azure doesn’t support the default Image Optimization
-  // },
-  // trailingSlash: true, // optional but helpful for static routing,
-  // assetPrefix: '/Site/', // For Site IIS Application
-  // basePath: '/Site',// For Site IIS Application
-  // // Normal assetPrefix: '', basePath not required
-  // env: {
-  //   NEXT_PUBLIC_BASE_PATH: "/Site",
-  // },
+ 
   reactStrictMode:true,
   output:"standalone",
+   experimental: {
+    useCache: true,
+  },
 
 };
 
