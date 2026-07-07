11f750c100dd2b668e5b9e4f21431fda619d8475
MohammedAlith1312 <m.aalith@faaztechsolutions.com>
2026-07-07 20:29:05 +0530

    Update next.config.ts (via Pages CMS)

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-
 // Point to your routing file
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig:NextConfig = {
- 
+
   reactStrictMode:true,
   // output:"standalone",//stand
- 
 
 };
-
 
 export default withNextIntl(nextConfig);
